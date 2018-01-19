f1de56c7c67ab7fd9b085cf230d829de4ba341b8
garojar <garojar@hotmail.com>
2018-01-19 07:28:14 -0300

    add validacion rut en model persona

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -26,4 +26,46 @@ namespace Lab.Models
 
         public List<Cotizacion> listCotizaciones {get; set;} 
     }
+
+
+
+    public static Boolean validarRut(String rut)
+    {
+		    Regex expresion = new Regex("^([0-9]+-[0-9K])$");
+		    
+            string dv = rut[rut.lenght-1];
+    		
+            if (!expresion.IsMatch(rut))
+            {
+			    return false;
+		    }
+
+		    //string[] rutSinDv = rut.Split('-');
+            int rutNum = int.Parse(rut.Split('-')[0])
+            string dvCalculado = '';
+
+            int sum = 0;
+		    int factor = 1;
+		    while (rut != 0) {
+		    	factor++;
+			    if (factor == 8)
+			        factor = 2;
+		        sum += (rutNum % 10)*factor;
+			    rutNum = rutNum / 10;
+		    }
+		    sum = 11 - (sum % 11);
+		    if (sum == 11)	{
+			    svCalculado="0";
+		    } elseif (sum==10) {
+			    dvCalculado="K";
+		    } else {
+			    dvCalculado=sum.ToString;
+		    }
+
+
+    		if (dv != dvCalculado) {
+			    return false;
+		    }
+		return true;
+    }
 }
