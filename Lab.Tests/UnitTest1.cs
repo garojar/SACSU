22dbb11707068b9c481b19707d3a09acf6e1d494
garojar <garojar@hotmail.com>
2018-01-19 09:15:20 -0300

    add test lab

--- Lab.Tests/UnitTest1.cs
+++ Lab.Tests/UnitTest1.cs
@@ -5,10 +5,18 @@ namespace Lab.Tests
 {
     public class UnitTest1
     {
+       /// <summary>
+        /// Test Unitario para rut sin guion
+        /// </summary>
         [Fact]
-        public void Test1()
+        public void retornarExcepcionPorGuionVerificador()
         {
+            var persona = new Persona();
+            persona.Rut = "194460880";
 
+            Exception ex = Assert.Throws<ArgumentException>(() => persona.validate());
+
+            Assert.Equal("RUT invalido", ex.Message);
         }
     }
 }
